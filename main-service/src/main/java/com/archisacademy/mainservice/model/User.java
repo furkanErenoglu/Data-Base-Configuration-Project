9aef93d7128723a4380c5216d5c0026a59cb4f77
furkanErenoglu <99097997+furkanErenoglu@users.noreply.github.com>
2023-10-18 12:54:12 +0300

    added model package

---
+++ main-service/src/main/java/com/archisacademy/mainservice/model/User.java
@@ -0,0 +1,30 @@
+package com.archisacademy.mainservice.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "lastName")
+    private String lastName;
+
+    @Column(name = "userName")
+    private String userName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+}
